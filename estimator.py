25e994ae6d57f3ab21ac0cee6bded5b4fcb2ec91
Aryan Dadwal <aryandadwal2006@gmail.com>
2025-11-17 08:53:15 +0530

    Add function to calculate total VRAM usage

--- estimator.py
+++ estimator.py
@@ -1,1 +1,10 @@
+def f(h: int, w: int, prompt_length: int, **kwargs):
+    model_weights = 2048 
+    framework_overhead = 600 
+    pixel_area = h * w
+    base_activation_memory = 18.53 * pixel_area / (1024**2)
+    prompt_factor = max(1.0, prompt_length / 77.0)
+    attention_memory = 1.3 * pixel_area / (1024**2) * prompt_factor
+    total_vram = model_weights + framework_overhead + base_activation_memory + attention_memory
+    return total_vram
 
